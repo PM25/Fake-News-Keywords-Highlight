1d8d2d4d9fdff51de3fb33771ab6d5c48d2aba8f
PY <pyhuang97@gmail.com>
2020-12-18 00:28:00 +0800

    only load data once to increase speed.

--- Fake_News_Detector/js/script.js
+++ Fake_News_Detector/js/script.js
@@ -19,13 +19,13 @@ const _ = undefined;
 // start from here
 function main() { 
     let confirm_btn = document.querySelector("#confirm-btn");
-    let checkbox1 = document.getElementById("fakeandrealnews"),
-        checkbox2 = document.getElementById("sourcebasedfakenews");
-    let scale = chroma.scale(['#eff8ff','#ff9b93']).mode('lch');
+    let scale = chroma.scale(['#d0e8f2', '#d0e8f2', '#e6e6e6', '#ff9b93']).mode('lch');
     let input_area = document.querySelector("#input-txt");
+    let database = {"data": "", "updating": false};
     
     confirm_btn.addEventListener("click", ()=>{
-        execute([checkbox1, checkbox2], input_area, scale);
+        update_data(database);
+        execute(input_area, scale, database);
     });
 
     document.getElementById("clear-btn").addEventListener("click", ()=>{
@@ -36,43 +36,56 @@ function main() {
         threshold_value = document.getElementById("threshold-value");
     threshold_slider.addEventListener("input", ()=>{
         threshold_value.innerHTML = threshold_slider.value / 100;
-        execute([checkbox1, checkbox2], input_area, scale);
+        execute(input_area, scale, database);
     });
 }
 
-function execute(checkboxs, input_area, scale) {
+function execute(input_area, scale, database) {
+    console.log(database.updating)
+    if(database.updating == true) {
+        setTimeout(() => { execute(input_area, scale, database); }, 100);
+    }
     input_area.innerHTML = input_area.innerText || input_area.textContent;
-    if(checkboxs[0].checked) {
-        tmp("data/Fake_and_real_news_dataset.json", input_area, scale);
-    }
-    if(checkboxs[0].checked) {
-        tmp("data/Source_based_Fake_News_Classification.json", input_area, scale);
-    }
-}
-
-function tmp(fname, input_area, scale) {
+    
     let threshold_slider = document.getElementById("threshold-slider");
     let threshold = threshold_slider.value / 100;
 
-    read_file(fname, (data)=>{
-        let txt = input_area.innerHTML;
-        let word_score = JSON.parse(data);
-        let unique_words = txt.split(' ').filter(onlyUnique);
-        unique_words.forEach((word) => {   
-            if(word_score[word.toLowerCase()] > threshold) {
-                // let scale_ratio = word_score[word];
-                let scale_ratio = (word_score[word]-threshold)/(1-threshold);
-                target_word = " " + word + " "
-                replace_word = " <span class='highlight' style='background:" + scale(scale_ratio).hex() + "'>" + word + "</span> "
-                input_area.innerHTML = input_area.innerHTML.replace(target_word, replace_word);
-            }
-        })
+    let txt = input_area.innerText || input_area.textContent;
+    let word_score = JSON.parse(database.data);
+    let unique_words = txt.match(/\b(\w+)\b/g).filter(onlyUnique);
+    unique_words.forEach((word) => {
+        if(word_score[word.toLowerCase()] >= threshold) {
+            let scale_ratio = word_score[word.toLowerCase()].toPrecision(2);
+            // let scale_ratio = (word_score[word]-threshold)/(1-threshold);
+            let color = scale(scale_ratio).hex();
+            let re = new RegExp("([^a-zA-Z<>])(" + word + ")([^a-zA-Z<>])","g");
+            input_area.innerHTML = input_area.innerHTML.replace(re, "\$1<span class='highlight' style='background:"+color+"'>\$2</span>\$3");
+        }
     });
 
     function onlyUnique(value, index, self) {
         return self.indexOf(value) === index;
     }
 }
+
+function update_data(database) {
+    database.updating = true;
+    let checkbox1 = document.getElementById("fakeandrealnews"),
+        checkbox2 = document.getElementById("sourcebasedfakenews");
+    if(checkbox1.checked) {
+        read_file("data/Fake_and_real_news_dataset.json", (data)=>{
+            database.data = data;
+            database.updating = false;
+        });
+    }
+    else if(checkbox2.checked) {
+        read_file("data/Source_based_Fake_News_Classification.json", (data)=>{
+            database.data = data;
+            database.updating = false;
+        })
+    }
+}
+
 
 // read external files
 function read_file(fpath, callback) {
